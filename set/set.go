a48f1462c1b578d3fe094a7f79ae730a1e79c72a
zanpatryk <patryk.zan@wp.pl>
2025-08-07 14:23:10 +0200

    feat:added implementation of creating sets from string slice + added tests

---
+++ set/set.go
@@ -0,0 +1,14 @@
+package set
+
+func CreateSet(s []string) []string {
+	seen := make(map[string]bool)
+	result := make([]string, 0, len(s))
+
+	for _, val := range s {
+		if !seen[val] {
+			seen[val] = true
+			result = append(result, val)
+		}
+	}
+	return result
+}
